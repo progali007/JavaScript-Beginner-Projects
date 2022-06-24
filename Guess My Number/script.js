23ae9a5965b73015e0dacf0afe44cabb2159b1d7
progali007 <code.progali@gmail.com>
2022-06-24 15:24:41 +0430

    Define Values

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const message = document.querySelector('.message'),
+   number = document.querySelector('.number'),
+   score = document.querySelector('.score'),
+   check = document.querySelector('.check'),
+   highScore = document.querySelector('.highscore'),
+   guess = document.querySelector('.guess');
