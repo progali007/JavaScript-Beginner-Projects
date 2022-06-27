babf0725d4ed8b1877fa9ac2d65f2f63a3cc714e
progali007 <code.progali@gmail.com>
2022-06-27 23:18:55 +0430

    Add some functionality

--- Guess My Number/script.js
+++ Guess My Number/script.js
@@ -1,8 +1,54 @@
 'use strict';
+const cl = console.log.bind(console);
 
+const body = document.getElementsByTagName('body');
 const message = document.querySelector('.message'),
    number = document.querySelector('.number'),
    score = document.querySelector('.score'),
    check = document.querySelector('.check'),
-   highScore = document.querySelector('.highscore'),
+   highScore = document.querySelector('.high-score'),
    guess = document.querySelector('.guess');
+
+const secretNumber = Math.floor(Math.random() * 20) + 1
+let userScore = 10;
+
+cl(secretNumber)
+check.addEventListener('click', () => {
+   const inputNumber = Number(guess.value);
+   let userMessage = ""
+
+   if (userScore === 1) {
+      score.innerHTML = 0;
+      message.innerHTML = '♨ You lost the game !';
+      return;
+   }
+   if (!inputNumber) {
+      message.innerHTML = '⛔ Enter a number pleas';
+      return;
+   }
+
+   /* Check Input */
+   if (inputNumber === secretNumber) {
+      userMessage = '🎉 Well Done';
+      body[0].classList.add('well-done');
+   }
+   else if (inputNumber >= secretNumber - 2 && inputNumber <= secretNumber + 2) {
+      userMessage = '😶 You are so close';
+      userScore--;
+
+   }
+   else if (inputNumber > secretNumber) {
+      userMessage = '📈 Too High';
+      userScore--;
+
+   }
+   else if (inputNumber < secretNumber) {
+      userMessage = '📉 Too Low';
+      userScore--;
+
+   }
+
+   score.innerHTML = userScore;
+   message.innerHTML = userMessage;
+
+})
